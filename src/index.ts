c823bbe6caaced74d43bfcdcc45dc948839156b8
jrsouzadev <jrsouzadev@gmail.com>
2020-11-02 21:55:06 -0300

    Improvements

--- src/index.ts
+++ src/index.ts
@@ -13,19 +13,3 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-const iuhisa = 'iwhdiuhdwid8u9ui3newodij'
-
-for (let i = 0; i < 10; i++) {
-  console.log(iuhisa)
-}
-
-const myFriend: User = {
-  name: 'Joao',
-  id: 'juanito'
-}
-console.log('AMENDOAAAAAASSS')
-console.log(myFriend)
-
-const count = 3 + 3
-console.log(count)
