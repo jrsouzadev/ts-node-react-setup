ca858bf74af8a71c913c4f071341b4609f6f8805
jrsouzadev <jrsouzadev@gmail.com>
2020-11-02 21:30:37 -0300

    Improvements

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import path from 'path'
+// eslint-disable-next-line no-unused-vars
+import { User } from './models/user'
 
 const app = express()
-
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'public')))
 }
@@ -11,3 +13,19 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const iuhisa = 'iwhdiuhdwid8u9ui3newodij'
+
+for (let i = 0; i < 10; i++) {
+  console.log(iuhisa)
+}
+
+const myFriend: User = {
+  name: 'Joao',
+  id: 'juanito'
+}
+console.log('AMENDOAAAAAASSS')
+console.log(myFriend)
+
+const count = 3 + 3
+console.log(count)
