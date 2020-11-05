3198b0ffe7bd787d650b78d60bf57124ffc428d9
jrsouzadev <jrsouzadev@gmail.com>
2020-11-05 01:56:42 -0300

    Added sockets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import socketioClient from 'socket.io-client';
+
+
+function useSocket() {
+  const [socket, setSocket] = useState<SocketIOClient.Socket | null>(null);
+
+  useEffect(() => {
+    let conn = socketioClient();
+    conn.on('connect', () => {
+      console.log(`Socket connected with id ${conn.id}`)
+    });
+    setSocket(conn);
+  }, []);
+
+  return socket;
+}
+
 
 function App() {
+  const socket = useSocket();
+
   return (
     <div>
       React App
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import express from 'express'
 import path from 'path'
+import http from 'http'
+import socketio from 'socket.io'
 
 const app = express()
 
-const publicFolder = path.join(__dirname, 'public')
-console.log(__dirname, publicFolder)
-
-console.log()
 if (process.env.NODE_ENV === 'production') {
+  const publicFolder = path.join(__dirname, 'public')
   app.use(express.static(publicFolder))
 }
 
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', socket => {
+  console.log(`New client connected with id ${socket.id}`)
+
+  socket.on('disconnect', () => {
+    console.log(`Client of id ${socket.id} has disconnected`)
+  })
+})
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
