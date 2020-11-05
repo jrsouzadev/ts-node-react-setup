3b4bf4d5ba3568a975037032cc264e89abc6dc46
jrsouzadev <jrsouzadev@gmail.com>
2020-11-05 11:04:27 -0300

    Create server component

--- src/index.ts
+++ src/index.ts
@@ -47,26 +47,3 @@ const publicFolder = path.join(__dirname, 'public')
 const server = new Server(publicFolder)
 
 server.listen()
-
-// const app = express()
-
-// if (process.env.NODE_ENV === 'production') {
-//   const publicFolder = path.join(__dirname, 'public')
-//   app.use(express.static(publicFolder))
-// }
-
-// const server = http.createServer(app)
-// const io = socketio(server)
-
-// io.on('connection', socket => {
-//   console.log(`New client connected with id ${socket.id}`)
-
-//   socket.on('disconnect', () => {
-//     console.log(`Client of id ${socket.id} has disconnected`)
-//   })
-// })
-
-// const PORT = process.env.PORT || 5000
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
